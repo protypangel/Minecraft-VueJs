806215b9fa9af16c2c4fa5f545079ff711acebf0
protypangel <mascret@et.esiea.fr>
2019-09-24 17:41:01 +0200

    script permettant d'initialiser tout le monde au meme nom de la branch donnée

---
+++ bash/InitializeAllBranch.bat
@@ -0,0 +1,85 @@
+:: Ne pas afficher les commandes et n'afficher que la commande tapez
+@echo off
+cls
+echo %0 %*
+:: Connaitre le nombre d'argument
+set argument=0
+for %%x in (%*) do Set /A argument+=1
+
+if %argument% equ 0 goto no_argument
+if %argument% equ 1 goto argument
+if %argument% gtr 1 goto error 
+
+exit
+
+:no_argument
+echo Quel est la branch qui initialisera tout le monde ?
+set /p new_argument=""
+goto script
+exit
+
+:argument
+set new_argument=%1
+goto script
+exit
+
+:script
+goto master
+exit
+
+:master
+if %new_argument% neq master (
+    goto amine
+) else (
+    git branch amine
+    git pull
+    git branch master
+    git branch natha
+    git pull
+    git branch master
+    git branch mehdi
+    git pull
+    git branch master
+)
+exit
+:amine
+if %new_argument% neq amine (
+    goto natha
+) else (
+    git branch natha
+    git pull
+    git branch amine
+    git branch mehdi
+    git pull
+    git branch amine
+)
+exit
+:natha
+if %new_argument% neq natha (
+    goto mehdi
+) else (
+    git branch amine
+    git pull
+    git branch natha
+    git branch mehdi
+    git pull
+    git branch natha
+)
+exit
+:mehdi
+if %new_argument% neq mehdi (
+    echo Votre argument n'es pas dans la liste definit
+    goto no_argument
+) else (
+    git branch amine
+    git pull
+    git branch master
+    git branch natha
+    git pull
+    git branch master
+)
+exit
+
+:error
+echo ERROR: Il n'y pas le bon nombre d'argument en entrer, merci de tapez:  .\bash\InitializeAllBranch.bat
+exit
