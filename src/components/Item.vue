14c4f9665fed63657ee1e44ab811be2214f78c66
amine970 <mouhammadoul-a@et.esiea.fr>
2019-10-07 11:40:49 +0200

    size image fixed, image-rendering: pixelated; pour pas que ça soit flou

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -3,12 +3,13 @@
   https://www.npmjs.com/package/minecraft-items
   https://forum.vuejs.org/t/decode-of-base-64-api-using-vuejs/18828
   https://github.com/dizhar/resize-base64
+  <img :src="'data:image.png;base64,' + pioche_p.icon">
+  <img src="./images/piocheDiamant.jpg" width="150">
  -->
 <template>
   <div>
       <div class="container" v-for="item in items" :key="item.id">
-        <img src="./images/piocheDiamant.jpg" width="150">
-        <img :src="'data:image.png;base64,' + pioche_p.icon">
+        <img :src="'data:image.png;base64,' + pioche_p.icon" width="150">
         <p>{{item.nom}}</p>
       </div>
   </div>
@@ -41,28 +42,29 @@ export default {
 </script>
 <style scoped>
 
-div .container {
-  border: 7px solid grey;
-  max-width: 15%;
-  height: auto;
-  width: auto;
-  margin: 20px;
-  border-radius: 10%;
-  box-sizing: border-box;
-  text-align: center;
-}
-div .container img {
-  margin-left: auto;
-  margin-right: auto;
-  display: block;
-}
-div .container p {
-  border: 4px solid green;
-  border-radius: 15%;
-  font-size: 1.5em;
-  display: inline-block;
-  padding: 3px;
-  box-sizing: border-box;
-  margin: 10px auto;
-}
+  div .container {
+    border: 7px solid grey;
+    max-width: 15%;
+    height: auto;
+    width: auto;
+    margin: 20px;
+    border-radius: 10%;
+    box-sizing: border-box;
+    text-align: center;
+  }
+  div .container img {
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+    image-rendering: pixelated;
+  }
+  div .container p {
+    border: 4px solid green;
+    border-radius: 15%;
+    font-size: 1.5em;
+    display: inline-block;
+    padding: 3px;
+    box-sizing: border-box;
+    margin: 10px auto;
+  }
 </style>
